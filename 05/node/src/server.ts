1a4eacbcfd3f151831e6c9df6d004518d70c69ed
wscnd <wellingson@gmail.com>
2021-04-21 11:41:18 -0300

    nlw05 - node - aula 2 - added migrations, repositories, entities, basic router

--- 05/node/src/server.ts
+++ 05/node/src/server.ts
@@ -1,15 +1,13 @@
+import "reflect-metadata";
 import express from 'express';
+import "./database";
+import { routes } from "./routes";
 
 const app = express();
 
+app.use(routes);
+
 const PORT = 3333;
 
-app.get("/", (req, res) => {
-    return res.send("Hello!");
-});
-
-app.post("/", (req, res) => {
-    return res.send("hello from POST!");
-});
 
 app.listen(PORT, () => { console.log(`running at port ${PORT}`); });
