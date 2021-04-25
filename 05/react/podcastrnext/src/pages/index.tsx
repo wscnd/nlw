e204a7b5c3d45f0a47f70f19a54d621b55f1d6ca
wscnd <wellingson@gmail.com>
2021-04-25 18:16:57 -0300

    nlw05 - react - getstaticprops good practices

--- 05/react/podcastrnext/src/pages/index.tsx
+++ 05/react/podcastrnext/src/pages/index.tsx
@@ -1,21 +1,66 @@
-export default function Home(props: {
-   episodes: Record<string, any>
-}): JSX.Element {
+import { GetStaticProps } from 'next'
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { api } from '../services/api'
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString'
+
+type Episode = {
+   id: string
+   title: string
+   members: string
+   published_at: string
+   thumbnail: string
+   description: string
+   file: {
+      url: string
+      type: string
+      duration: string
+   }
+}
+
+type Data = {
+   data: Episode[]
+}
+
+type HomeProps = {
+   episodes: Array<Episode>
+}
+
+export default function Home(props: HomeProps): JSX.Element {
    return (
       <div>
-         <h1>Index</h1>
          <pre>{JSON.stringify(props.episodes, null, 2)}</pre>
       </div>
    )
 }
 
-export async function getStaticProps() {
-   const response = await fetch('http://localhost:3333/episodes')
-   const data = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+   const { data }: Data = await api.get('episodes', {
+      params: {
+         _limit: 12,
+         _sort: 'published_at',
+         _order: 'desc'
+      }
+   })
+
+   const episodes = data.map((episode) => {
+      return {
+         id: episode.id,
+         title: episode.title,
+         thumbnail: episode.thumbnail,
+         members: episode.members,
+         publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {
+            locale: ptBR
+         }),
+         duration: convertDurationToTimeString(Number(episode.file.duration)),
+         url: episode.file.url,
+         description: episode.description
+      }
+   })
 
    return {
       props: {
-         episodes: data
+         episodes
       },
       revalidate: 60 * 60 * 8
    }
---
+++ 05/react/podcastrnext/src/utils/convertDurationToTimeString.ts
@@ -0,0 +1,10 @@
+export function convertDurationToTimeString(duration: number) {
+   const hours = Math.floor(duration / 3600)
+   const minutes = Math.floor((duration % 3600) / 60)
+   const seconds = duration % 60
+
+   const timeString = [hours, minutes, seconds]
+      .map((unidade) => String(unidade).padStart(2, '0'))
+      .join(':')
+   return timeString
+}
