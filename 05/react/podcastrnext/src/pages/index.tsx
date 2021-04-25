f0c555583e57200b1a1592281e05bd7548d8cf85
wscnd <wellingson@gmail.com>
2021-04-24 23:13:20 -0300

    nlw05 - react - api & ssgs

--- 05/react/podcastrnext/src/pages/index.tsx
+++ 05/react/podcastrnext/src/pages/index.tsx
@@ -1,3 +1,22 @@
-export default function Home(): JSX.Element {
-   return <h1>Index</h1>
+export default function Home(props: {
+   episodes: Record<string, any>
+}): JSX.Element {
+   return (
+      <div>
+         <h1>Index</h1>
+         <pre>{JSON.stringify(props.episodes, null, 2)}</pre>
+      </div>
+   )
+}
+
+export async function getStaticProps() {
+   const response = await fetch('http://localhost:3333/episodes')
+   const data = await response.json()
+
+   return {
+      props: {
+         episodes: data
+      },
+      revalidate: 60 * 60 * 8
+   }
 }
